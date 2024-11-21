3bea44f27094609ca1323915d88d02b092004d92
Dusuan <XxDusuanxX@hotmail.com>
2024-11-21 12:18:15 -0800

    Prueba

---
+++ hospitalservice/src/app/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-white h-full w-full">
+      <h6>Emiliano se la come</h6>
+    </div>
+  );
+}
