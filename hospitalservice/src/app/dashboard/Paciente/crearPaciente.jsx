a5c372a1e2227b683650a69def8bf01956e384c1
Emiliano0X1 <egp2506@gmail.com>
2024-12-01 23:57:14 -0800

    hola

---
+++ hospitalservice/src/app/dashboard/Paciente/crearPaciente.jsx
@@ -0,0 +1,153 @@
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { useState } from 'react';
+
+
+const crearPaciente = async (nombre,telefono,razonDeVisita,citaId) => {
+    try {
+      console.log(nombre)
+      console.log(telefono)
+
+      const response = await fetch(`http://192.168.1.73:8080/api/v1/paciente/${citaId}`, {
+          method: 'POST',
+          headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+          body: JSON.stringify({
+            nombre : nombre,
+            telefono : telefono,
+            razonDeVisita : razonDeVisita
+          }),
+      });
+  
+      console.log('Response Status:', response.status);
+  
+      if(!response.ok){
+        const errorData = await response.json()
+        console.log("No funciono correctamente", errorData);
+
+      }
+  
+      else{
+        const result = await response.json();
+        console.log("Se creo al Paciente Exitosamente",result)
+  
+      }
+  
+    } catch (error) {
+      console.log("Error fatal en el sistema")
+    }
+  
+  };
+
+
+export default function FormDialog() {
+
+
+  const [open, setOpen] = React.useState(false);
+
+  const [nombre,setNombre] = useState('');
+  const [telefono,setTelefono] = useState('');
+  const [razonDeVisita,setRazonDeVisita] = useState('');
+  const [citaId,setCitaId] = useState('');
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <React.Fragment>
+      <Button variant="outlined" onClick={handleClickOpen}>
+        Crear Paciente
+      </Button>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          component: 'form',
+          onSubmit: (event) => {
+            event.preventDefault();
+            const formData = new FormData(event.currentTarget);
+            const formJson = Object.fromEntries(formData.entries());
+            const email = formJson.email;
+            console.log(email);
+            handleClose();
+          },
+        }}
+      >
+        <DialogTitle>Crear Paciente : DAY ONE</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            Por favor llene los datos que se requieren
+          </DialogContentText>
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="nombre"
+            label="Nombre"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setNombre(e.target.value)}
+
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="telefono"
+            label="Telefono"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setTelefono(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="razonDeVisita"
+            label="Razon De Visita"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setRazonDeVisita(e.target.value)}
+          />
+          <TextField
+            autoFocus
+            required
+            margin="dense"
+            id="name"
+            name="cita_id"
+            label="Cita Id"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(e) => setCitaId(e.target.value)}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancelar</Button>
+          <Button type="submit" onClick={() => agendarCita(nombre,telefono,razonDeVisita,citaId)}>Crear</Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
--- hospitalservice/src/app/dashboard/Paciente/page.jsx
+++ hospitalservice/src/app/dashboard/Paciente/page.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
@@ -5,17 +7,60 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import { Typography } from "@mui/material";
+import { EditRoad } from "@mui/icons-material";
+import CitaCard from "../../components/CitaCard";
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material"
+import FormDialog from "../Citas/agendarCita"
 
 export default function Home() {
+
+  const [pacientes,setPacientes] = useState([]);
+
+  const fetchPaciente = async() => {
+      try{
+        const response = await fetch("http://192.168.1.73:8080/api/v1/paciente")
+        
+        if(!response.ok){
+          throw new Error("Error en la obtencion de datos")
+        }
+
+        const data = await response.json();
+        setPacientes(data);
+      }catch(error){
+        console.log("Error fatal en el sistema", error)
+      }
+
+  };
+
+  useEffect(() => {
+    fetchPaciente();
+  },[]);
+
+
   return (
     <Box className="bg-white w-full h-full">
       <Box className="p-6 h-full">
-        <Card className="h-full text-black">
+        <Card className="h-full overflow-scroll text-black">
+          <FormDialog></FormDialog>
+
+          <Button
+              className="mr-4 text-black bg-white"
+              size="large"
+              variant="contained"
+              onClick={fetchPaciente}
+            >
+              Refresh
+            </Button>
+
           <CardContent>
-            <Typography variant="h1">Pacientes</Typography>
+            {pacientes.map((paciente, index) => {
+              return <PacienteCard key={index} paciente={paciente} />;
+            })}
           </CardContent>
         </Card>
       </Box>
     </Box>
   );
 }
+
